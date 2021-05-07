1a59ee3ee911b28d1adf612a9c1a447e9e26c0f0
Dan Lewer <danlewer@users.noreply.github.com>
2021-05-07 13:09:07 +0100

    Update fig_S4.R

--- fig_S4.R
+++ fig_S4.R
@@ -5,7 +5,8 @@ library(devEMF)
 d <- read.csv("https://raw.githubusercontent.com/danlewer/hospital_admissions_opioid_deaths/main/conditional_odds_ratios_for_chart.csv", col.names = c('m', 'g', 'e', 'l', 'u'))
 setDT(d)
 d[, c('e', 'l', 'u') := lapply(.SD, log), .SDcols = c('e', 'l', 'u')]
-model_order <- c('standard', 'short', 'long', 'endDeath', 'end2019', 'ahm', paste0('farr', 1:10), 'farr_ma')
+d <- d[m != 'ahm']
+model_order <- c('standard', 'short', 'long', 'endDeath', 'end2019', paste0('farr', 1:10), 'farr_ma')
 d[, m := factor(m, model_order)]
 d <- d[order(g, m)]
 
@@ -29,8 +30,8 @@ d[, o := max(o) - o]
 
 d[, cols := as.character(m)]
 d$cols[grepl('farr', d$cols)] <- 'farr'
-col7 <- brewer.pal(7, 'Set2')
-d[, cols := factor(cols, c('standard', 'short', 'long', 'endDeath', 'end2019', 'ahm', 'farr'), col7)]
+col6 <- brewer.pal(7, 'Set2')[c(1:5, 7)]
+d[, cols := factor(cols, c('standard', 'short', 'long', 'endDeath', 'end2019', 'farr'), col6)]
 d[, cols := as.character(cols)]
 d$cols[d$m == 'farr_ma'] <- NA
 
@@ -48,8 +49,9 @@ emf('fig_S4.emf', height = 8, width = 10, family = 'Corbel')
 
 par(xpd = NA, mar = c(7, 7, 0, 20))
 plot(1, type = 'n', xlim = xlims, ylim = ylims, axes = F, xlab = NA, ylab = NA)
-rect(xlims[1], ylims[1], xlims[2], ylims[2])
-segments(xlims[1], group_seps, xlims[2], group_seps)
+#rect(xlims[1], ylims[1], xlims[2], ylims[2])
+segments(xlims[1] * 1.3, group_seps, xlims[2], group_seps)
+segments(xlims[1] * 1.3, ylims, x1 = xlims[2])
 segments(0, ylims[1], y1 = ylims[2])
 with(d, {
   points(e, o, col = cols, pch = 19, cex = size)
@@ -61,29 +63,43 @@ text(xlims[1] * 1.1, ylabs, c('A: Days 1-14\nof admission',
                               'D: Days 3-14\nafter discharge'), adj = 1)
 axis(1, at = log(xlabs), labels = xlabs, pos = ylims[1])
 for(i in 1:2) {
-  with(d[m == 'farr_ma'][i], polygon(x = c(l, e, u, e), y = c(o, o-farr_ma_interval, o, o+farr_ma_interval), border = col7[7], col = col7[7]))
+  with(d[m == 'farr_ma'][i], polygon(x = c(l, e, u, e), y = c(o, o-farr_ma_interval, o, o+farr_ma_interval), border = col6[6], col = col6[6]))
 }
 text(mean(xlims), ylims[1] - 12, 'Conditional odds ratio\nof opioid-related death\n(reference = time in the community)')
 
 # legend
 
-ys <- seq(65, 15, length.out = 7)
-arrows(3, ys[-7], 3.4, ys[-7], code = 3, angle = 90, length = 0.07, col = col7[-7])
-points(rep(3.2, 6), ys[-7], col = col7[-7], pch = 19)
+ys <- seq(55, 15, length.out = 6)
+arrows(3, ys[-6], 3.4, ys[-6], code = 3, angle = 90, length = 0.07, col = col6[-6])
+points(rep(3.2, 5), ys[-6], col = col6[-6], pch = 19)
 
-text(3.7, ys[-7], c('Control days sampled from\n730-28 days before death\n(main analysis)',
+text(3.7, ys[-6], c('Control days sampled from\n730-28 days before death\n(main analysis)',
                     'Control days sampled from\n365-28 days before death',
                     'Control days sampled from\n1095-28 days before death',
                     'Standard self-controlled case series;\nend follow-up at death',
-                    'Standard self-controlled case series;\nend follow-up at 31 December 2019',
-                    'Analysis limited to deaths with\ndiagnoses of aspiration pneumonia\nor hypoxia'),
+                    'Standard self-controlled case series;\nend follow-up at 31 December 2019'),
      adj = 0)
 
-ys <- seq(5, 10, length.out = 7)
+ys <- seq(7, 12, length.out = 7)
 xs <- seq(3.1, 3.3, length.out = 5)
-points(xs, ys[-(1:2)], pch = 19, cex = 0.5, col = col7[7])
-arrows(xs - 0.2, ys[-(1:2)], xs + 0.2, ys[-(1:2)], length = 0.03, angle = 90, code = 3, col = col7[7])
+points(xs, ys[-(1:2)], pch = 19, cex = 0.5, col = col6[6])
+arrows(xs - 0.2, ys[-(1:2)], xs + 0.2, ys[-(1:2)], length = 0.03, angle = 90, code = 3, col = col6[6])
 text(3.7, mean(ys), 'Farrington model for event-\ndependent exposures, on 10\nsamples of 500 cases. Diamond\nshows pooled value from fixed-\neffects meta-analysis', adj = 0)
-polygon(c(min(xs), mean(xs), max(xs), mean(xs)), c(mean(ys[1:2]), ys[1], mean(ys[1:2]), ys[2]), col = col7[7], border = col7[7])
+polygon(c(min(xs), mean(xs), max(xs), mean(xs)), c(mean(ys[1:2]), ys[1], mean(ys[1:2]), ys[2]), col = col6[6], border = col6[6])
 
 dev.off()
+
+# -------------
+# results table
+# =============
+
+results_table <- d[!(m %in% paste0('farr', 1:10))]
+vals <- c('e', 'l', 'u')
+f <- function(x) format(round(exp(x), 2), nsmall = 2, digits = 2)
+results_table[, (vals) := lapply(.SD, f), .SDcols = vals]
+results_table[, q := paste0(e, ' (', l, '-', u, ')')]
+results_table[, q := gsub(' ', '', q)]
+results_table[, q := gsub('\\(', ' (', q)]
+results_table <- dcast(results_table, m ~ g, value.var = 'q')
+
+fwrite(results_table, 'alternative_models_results_table.csv')
