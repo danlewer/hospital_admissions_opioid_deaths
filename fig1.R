2df868da8cb0610bea49bf6bf320c8d24af6742a
Dan Lewer <danlewer@users.noreply.github.com>
2021-05-07 14:29:00 +0100

    Update fig1.R

--- fig1.R
+++ fig1.R
@@ -10,7 +10,7 @@ z <- seq_len(nboxes)-1
 
 yb <- rev(z + z/ratio)
 yt <- yb + 1
-  
+
 mp <- rowMeans(cbind(yt, yb))
 
 xp <- c(0, 2, 2.5, 4.5)
@@ -30,14 +30,14 @@ arrows(xm1, yb[-length(yb)], xm1, yt[-1], length = 0.1)
 arrows(xp[2], mp[-length(yb)], xp[3], length = 0.1)
 
 text(xm1, mp, c('All opioid-related deaths in England\n1 Jan 2010-31 Dec 2019 (published data)\n16,586',
-              'Individuals with a linked record in\nhospital databases\n14,629',
-              'Age 18-64\n13,647',
-              'No conflict recorded during linkage\n13,639',
-              'No conflict between dates of death\nrecorded in hospital and mortality data\n13,611'))
+                'Individuals with a linked record in\nhospital databases\n14,627',
+                'Age 18-64\n13,645',
+                'No conflict recorded during linkage\n13,637',
+                'No conflict between dates of death\nrecorded in hospital and mortality data\n13,609'))
 
-text(xm2, mp[-length(yb)], c('Deaths among individuals without a\nlinked record in hospital databases\n(estimate)\n1,957',
-                           'Aged <18 or 65+ at death\n982',
-                           'Possible error in date of death\nand/or final hospital admission\nrecorded during linkage process\n8',
-                           'Admission with discharge date >3 days\nafter death date\n28'))
+text(xm2, mp[-length(yb)], c('Deaths among individuals without a\nlinked record in hospital databases\n(estimate)\n1,959',
+                             'Aged <18 or 65+ at death\n982',
+                             'Possible error in date of death\nand/or final hospital admission\nrecorded during linkage process\n8',
+                             'Admission with discharge date >3 days\nafter death date\n28'))
 
 dev.off()
